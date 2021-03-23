a354da6b4da3573e3cf5a2c008a01347f4ab4fd0
arvind ramadurai <arvind.r@browserstack.com>
2021-03-23 16:37:16 +0530

    offers test fail

--- cypress/integration/offers/offersforMumbai.spec.ts
+++ cypress/integration/offers/offersforMumbai.spec.ts
@@ -29,16 +29,17 @@ describe('Offers for Mumbai geo-location', () => {
   
     it('Perform Login', () => {
 
+        cy.intercept('POST', Cypress.config().baseUrl + 'api/signin').as('signin')
         cy.get('#username', { timeout: 30000 }).should('be.visible').click({force:true}).type(user.user4.username +'{enter}'); 
         cy.get('#password', { timeout: 30000 }).click({force:true}).type(user.user4.password +'{enter}');
         cy.get('#login-btn', { timeout: 30000 }).click();
+        cy.wait('@signin', {timeout: 30000});
     
     })
 
     it('Check Offers available or not', () => {
 
-        cy.intercept('POST', Cypress.config().baseUrl + 'api/signin').as('signin')
-        // cy.wait('@signin', {timeout: 30000});
+        cy.get('.shelf-item__price', { timeout: 5000 }).should('be.visible'); // this should fail
 
     })
   
