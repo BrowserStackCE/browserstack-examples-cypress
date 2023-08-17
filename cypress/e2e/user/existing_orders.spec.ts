7027c754055a8a1468df10735ddfe3c162936f14
Abhi Singh <abhi.s@browserstack.com>
2023-08-18 01:23:25 +0530

    Updated cypress to v12

--- cypress/e2e/user/existing_orders.spec.ts
+++ cypress/e2e/user/existing_orders.spec.ts
@@ -13,40 +13,16 @@ describe('Login as User with existing Orders', function () {
     cy.visit('/');
     cy.wait('@apiCheck');
 
-  })
-
-  it('Click on Sign In link', () => {
-
     cy.intercept('GET', Cypress.config().baseUrl + '_next/static/chunks/pages/signin**').as('signinCheck')
     cy.get('#signin', { timeout: 30000 }).click();
     cy.wait('@signinCheck', { timeout: 30000 });
-
-  })
-
-  it('Perform Login', function () {
 
     cy.get('#username', { timeout: 30000 }).should('be.visible').click({ force: true }).type(user.user3.username + '{enter}');
     cy.get('#password', { timeout: 30000 }).click({ force: true }).type(user.user3.password + '{enter}');
     cy.get('#login-btn', { timeout: 30000 }).click();
 
-  })
-
-  it('Add items to Favourites', () => {
-
-    cy.get('#4', { timeout: 30000 }).should('be.visible').children('.shelf-stopper').children('button').click({ force: true });
-    cy.get('#5').children('.shelf-stopper').children('button').click({ force: true });
-
-  })
-
-  it('Click on "Favourites" link', () => {
-
-    cy.get('#favourites', { timeout: 30000 }).click();
-
-  })
-
-  it('Should see elements in the list', () => {
-
-    cy.get('.shelf-item', { timeout: 30000 }).should('be.visible').its('length').should('be.gt', 0);
+    cy.get('#orders', { timeout: 30000 }).should('be.visible').click();
+    cy.get('.a-box-group.a-spacing-base.order', { timeout: 30000 }).should('be.visible').its('length').should('be.gt', 0);
 
   })
 
--- cypress/e2e/user/image_not_loading.spec.ts
+++ cypress/e2e/user/image_not_loading.spec.ts
@@ -13,25 +13,13 @@ describe('Login as User with no image loaded', function () {
     cy.visit('/');
     cy.wait('@apiCheck');
 
-  })
-
-  it('Click on Sign In link', () => {
-
     cy.intercept('GET', Cypress.config().baseUrl + '_next/static/chunks/pages/signin**').as('signinCheck')
     cy.get('#signin', { timeout: 30000 }).click();
     cy.wait('@signinCheck', { timeout: 30000 });
 
-  })
-
-  it('Perform Login', function () {
-
     cy.get('#username', { timeout: 30000 }).should('be.visible').click({ force: true }).type(user.user2.username + '{enter}');
     cy.get('#password', { timeout: 30000 }).click({ force: true }).type(user.user2.password + '{enter}');
     cy.get('#login-btn', { timeout: 30000 }).click();
-
-  })
-
-  it('Should see no image loaded', () => {
 
     cy.get('.shelf-item__thumb', { timeout: 30000 }).should('be.visible').children('img[src=""]').its('length').should('equal', 0);
 
